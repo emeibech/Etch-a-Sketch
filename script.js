2f599392028debcd590c35f512309431a7a66706
emeibech <emeibech@gmail.com>
2022-07-30 04:44:30 +0800

    Add coloring function when mouse is pressed

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //create grid of boxes
-let total = 9999;
+let total = 6400;
 const container = document.querySelector('.container');
 for (let i = 0; i < total; i++) {
     let boxes = document.createElement('div');
@@ -7,25 +7,39 @@ for (let i = 0; i < total; i++) {
     container.appendChild(boxes);
 }
 
-//ask user how many boxes they want for each side
-const input = document.querySelector('.width-and-height');
-input.addEventListener('submit', (e) => {
-    e.preventDefault();
+//change the number of boxes per side based on user selection
+const input = document.querySelector('#input');
+input.addEventListener('change', (e) => {
 
+    //remove previous grid
     for (let i = 0; i < total; i++) {
         document.querySelector('.container > div').remove();
     }
 
-    let width = document.querySelector('#width').value;
-    let height = document.querySelector('#height').value;
+    //get the new value for number of boxes
+    let value = e.target.value;
+    let width = Math.sqrt(value);
+    let height = Math.sqrt(value);
+    total = value;
+    
     container.setAttribute('style', `grid-template: repeat(${width}, minmax(auto, auto)) / repeat(${height}, minmax(auto, auto))`);
-    console.log(total);
-    total = width * height;
 
     //set new grid
     for (let i = 0; i < total; i++) {
         let boxes = document.createElement('div');
         boxes.classList.toggle('boxes');
         container.appendChild(boxes);
-    }
+    }}
+);
+
+//change color of box when user presses mouse over it
+function colorBlack (e) {
+    e.target.style.cssText = 'background-color: black';
+    container.addEventListener('mousemove', colorBlack);
+}
+container.addEventListener('mousedown', colorBlack);
+
+//stop coloring when user releases the mouse
+container.addEventListener('mouseup', function() {
+    container.removeEventListener('mousemove', colorBlack);
 });
