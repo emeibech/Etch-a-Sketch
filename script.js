5804cfdc0cdcc6566739b42ec82abdfc61389fad
emeibech <emeibech@gmail.com>
2022-07-31 15:39:33 +0800

    Make buttons functional

--- script.js
+++ script.js
@@ -32,14 +32,68 @@ input.addEventListener('change', (e) => {
     }}
 );
 
-//change color of box when user presses mouse over it
-function colorBlack (e) {
-    e.target.style.cssText = 'background-color: black';
-    container.addEventListener('mousemove', colorBlack);
+//set drawing color to black for when user presses mouse over the boxes
+function stopColorBlack () {
+    container.removeEventListener('mousemove', colorBlack);
 }
+
+function stopColorWhite () {
+    container.removeEventListener('mousemove', colorWhite);
+}
+
+function colorBlack (e) {
+    e.target.classList.add('boxes-black-fill')
+    container.addEventListener('mousemove', colorBlack);
+
+    //stop coloring black when user releases the mouse
+    container.addEventListener('mouseup', stopColorBlack);
+}
+
+function colorWhite (e) {
+    e.target.classList.remove('boxes-black-fill');
+    container.addEventListener('mousemove', colorWhite);
+
+    //stop coloring white when user releases the mouse
+    container.addEventListener('mouseup', stopColorWhite);
+}
+
 container.addEventListener('mousedown', colorBlack);
 
-//stop coloring when user releases the mouse
-container.addEventListener('mouseup', function() {
-    container.removeEventListener('mousemove', colorBlack);
-});
+//set drawing color to white for when user click erase
+// const draw = document.querySelector('.draw');
+// const erase = document.querySelector('.erase');
+function mode (e) {
+    if (e.target.className === 'draw') {
+        container.removeEventListener('mousedown', colorWhite);
+        container.addEventListener('mousedown', colorBlack);
+    }
+    else if (e.target.className === 'erase') {
+        container.removeEventListener('mousedown', colorBlack);
+        container.addEventListener('mousedown', colorWhite);
+    }
+    else if (e.target.className === 'clear') {
+        const boxes = document.querySelectorAll('.boxes');
+        for (box of boxes) {
+            box.classList.remove('boxes-black-fill');
+        }
+        container.addEventListener('mousedown', colorBlack);
+    }
+}
+
+const buttons = document.querySelectorAll('.buttons > button');
+buttons.forEach(button => button.addEventListener('click', mode));
+
+
+// draw.addEventListener('click', e => {
+//     container.removeEventListener('mousemove', colorWhite);
+//     container.addEventListener('mousedown', colorBlack);
+//     console.log(e.target.classList)
+//     });
+
+// erase.addEventListener('click', e => {
+//     container.removeEventListener('mousemove', colorBlack);
+//     container.addEventListener('mousedown', colorWhite);
+//     console.log(e.target.classList)
+//     });
+
+    //try nodelist
